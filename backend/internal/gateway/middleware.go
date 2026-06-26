65750589cc1554a1213faed95d990732b7313503
Jerome Chua <jeromechua.jz@gmail.com>
2026-06-27 00:19:17 +0800

    feat: add CORS middleware

---
+++ backend/internal/gateway/middleware.go
@@ -0,0 +1,20 @@
+package gateway
+
+import (
+	"net/http"
+)
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3030")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
+		next.ServeHTTP(w, r)
+	})
+}
